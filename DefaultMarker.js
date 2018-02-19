b337bd576139166242d13105bcd8f215807dd397
eabait <estebanabait@gmail.com>
2018-02-19 10:16:04 -0300

    Fixes PropTypes issues

--- DefaultMarker.js
+++ DefaultMarker.js
@@ -1,5 +1,5 @@
-import React, { PropTypes } from 'react';
-
+import React from 'react';
+import PropTypes from 'prop-types';
 import { View, StyleSheet, Platform, TouchableHighlight, ViewPropTypes } from 'react-native';
 
 export default class DefaultMarker extends React.Component {
